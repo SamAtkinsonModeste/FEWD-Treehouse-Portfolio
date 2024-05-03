e34ef98bb147b1f3e5f281d0a6f621f35a725d04
Samantha Atkinson <sammodeste@yahoo.com>
2024-05-04 00:38:59 +0100

    all animation done

--- js/app.js
+++ js/app.js
@@ -14,71 +14,268 @@ console.log(indicatDots)
 
 document.addEventListener('load', initializeElements)
 
+//sets up my elements
 function initializeElements () {
   overlay.style.display = 'block'
   projectSlider.style.display = 'flex'
   slideNav.style.display = 'flex'
 }
 
+//callback for when the animaLogo is finished
 const overlayDismiss = () => {
   overlay.style.display = 'none'
+  homeAni
+    .from(
+      '#giltter',
+      {
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      0
+    )
+    .from(
+      '#giltter-port-bg',
+      {
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      0
+    )
+
+    .from(
+      '.hori',
+      {
+        x: 400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      0.5
+    )
+    .from(
+      '.hori-port',
+      {
+        x: 400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      0.5
+    )
+    .from(
+      '.vert',
+      {
+        y: 400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      1
+    )
+    .from(
+      '.vert-port',
+      {
+        y: 400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      1
+    )
+    .from(
+      '.sam-img-home',
+      {
+        x: 400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      1.5
+    )
+    .from(
+      '.nam-colour',
+      {
+        x: -400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      1
+    )
+    .from(
+      '.nam-gold',
+      {
+        x: -400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      1.5
+    )
+    .from(
+      '.creative-text',
+      {
+        x: -400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      2
+    )
+    .from(
+      '.webDev-text',
+      {
+        x: -400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      2.5
+    )
+    .from(
+      '.head-port',
+      {
+        x: -400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      2.5
+    )
+    .from(
+      '.slide-nav',
+      {
+        y: -400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      2.5
+    )
+    .from(
+      '.projects-container',
+      {
+        y: -400,
+        opacity: 0,
+        stagger: { each: 0.1, from: 'start' },
+        ease: 'bouce.InOut',
+        duration: 2.5,
+        // repeat: 1,
+        yoyo: true
+        // repeatDelay:
+      },
+      2.5
+    )
 }
-
 const animaLogo = gsap.timeline({ onComplete: overlayDismiss })
-animaLogo.from(
-  '.s-down',
-  {
-    y: 400,
-    opacity: 0,
-    ease: 'bouce.InOut',
-    duration: 2.5,
-    repeat: 1,
-    yoyo: true,
-    repeatDelay: 2
-  },
-  1
-)
+const homeAni = gsap.timeline()
 
-animaLogo.from(
-  '.s-up',
-  {
-    y: -400,
-    opacity: 0,
-    ease: 'bouce.InOut',
-    duration: 2.5,
-    repeat: 1,
-    yoyo: true,
-    repeatDelay: 2
-  },
-  1
-)
-
-animaLogo.from(
-  '.a-left',
-  {
-    x: -400,
-    opacity: 0,
-    stagger: { each: 0.1, from: 'end' },
-    ease: 'bouce.InOut',
-    duration: 2.5,
-    repeat: 1,
-    yoyo: true,
-    repeatDelay: 3
-  },
-  0
-)
-
-animaLogo.from(
-  '.m-right',
-  {
-    x: 400,
-    opacity: 0,
-    stagger: { each: 0.1, from: 'start' },
-    ease: 'bouce.InOut',
-    duration: 2.5,
-    repeat: 1,
-    yoyo: true,
-    repeatDelay: 3
-  },
-  0
-)
+animaLogo
+  .from(
+    '.s-down',
+    {
+      y: 400,
+      opacity: 0,
+      ease: 'bouce.InOut',
+      duration: 2.5,
+      repeat: 1,
+      yoyo: true,
+      repeatDelay: 2
+    },
+    1
+  )
+  .from(
+    '.s-up',
+    {
+      y: -400,
+      opacity: 0,
+      ease: 'bouce.InOut',
+      duration: 2.5,
+      repeat: 1,
+      yoyo: true,
+      repeatDelay: 2
+    },
+    1
+  )
+  .from(
+    '.a-left',
+    {
+      x: -400,
+      opacity: 0,
+      stagger: { each: 0.1, from: 'end' },
+      ease: 'bouce.InOut',
+      duration: 2.5,
+      repeat: 1,
+      yoyo: true,
+      repeatDelay: 3
+    },
+    0
+  )
+  .from(
+    '.m-right',
+    {
+      x: 400,
+      opacity: 0,
+      stagger: { each: 0.1, from: 'start' },
+      ease: 'bouce.InOut',
+      duration: 2.5,
+      repeat: 1,
+      yoyo: true,
+      repeatDelay: 3
+    },
+    0
+  )
